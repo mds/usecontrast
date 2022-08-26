11d261d776bf18cad0810b0701e9e4e43dab9c67
mds <matt@mds.is>
2022-08-26 17:20:17 -0400

    cleaned up code and added css variables

--- src/js/contrast.js
+++ src/js/contrast.js
@@ -2,54 +2,56 @@
 
 $( document ).ready(function() {
 
-	// plugin 
-	const htmlEl = document.documentElement, outputExample = document.getElementById('output');
-	let savedTheme = 'light';
+	const modeSwitcher = document.querySelector(".plugin");
+	const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+	const currentMode = localStorage.getItem("mode");
 
-	// Toggle the theme and update their local storage.
-	toggleTheme = (bool) => {
-	   const theme = bool ? 'light' : 'dark';
-	   htmlEl.dataset.theme = theme;
-	   // localStorage.setItem('savedTheme', theme);
-	   outputExample.innerText = `${theme} theme`;
+	autoHover = function() {
+    $(".line-item").mouseover().delay( 100 );
 	}
 
-	// Handle their saved preferred theme.
-	setSavedTheme = () => {
-	  if (localStorage.getItem('savedTheme') === null) {
-	    localStorage.setItem('savedTheme', savedTheme);
+	// fires only on page load
+	if (currentMode == "dark") {
+		autoHover();
+	  document.body.classList.toggle("dark-mode");
+	} else if (currentMode == "light") {
+		autoHover();
+	  document.body.classList.toggle("light-mode");
+	}
+
+	// only fires one of these based on operating system preference
+	modeSwitcher.addEventListener("click", function () {
+	  if (prefersDarkMode.matches) {
+	  	autoHover();
+	    document.body.classList.toggle("light-mode");
+	    var mode = document.body.classList.contains("light-mode")
+	      ? "light"
+	      : "dark";
 	  } else {
-	    savedTheme = localStorage.getItem('savedTheme');
+	  	autoHover();
+	    document.body.classList.toggle("dark-mode");
+	    var mode = document.body.classList.contains("dark-mode")
+	      ? "dark"
+	      : "light";
 	  }
-	  htmlEl.dataset.theme = savedTheme;
-	  outputExample.innerText = `${savedTheme} theme`;
+	  localStorage.setItem("mode", mode);
+	});
+
+	setLight = function() {
+		$('.foreground p').html('foo');
+		$('.background p').html('bar');
 	}
 
-	// Set the default.
-	setSavedTheme();
-
-	
-
-	// dynamic copyright in the footer
-	var now = new Date();
-	var year = now.getFullYear();
-	$('.copyright').text('©'+year);
-
-	var dateObj = new Date();
-	var month = dateObj.getUTCMonth() + 1; //months from 1-12
-	var day = dateObj.getUTCDate();
-	var year = dateObj.getUTCFullYear();
-
-	newdate = day + " " + GetMonthName(month) + ", " + year;
-	newdatePlusYear = day + " " + GetMonthName(month) + ", " + (year+1);
-
-	function GetMonthName(monthNumber) {
-			var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-			return months[monthNumber - 1];
+	setDark = function() {
+		$('.foreground p').html('dark');
+		$('.background p').html('mode');
 	}
 
 });
 
+
+// $(".foreground p").html("111111");
+// $(".background p").html("FFFF00");	
 
 
 
